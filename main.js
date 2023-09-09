2785d4544c99cf57aca362bd1af77ed18a569d83
jichani <jichan7571@naver.com>
2023-09-09 23:15:28 +0900

    일단 로컬에서는 성공. 새로고침 안되게 AJAX 요청도 추가

--- main.js
+++ main.js
@@ -1,26 +1,56 @@
 const express = require("express");
 const mysql = require("mysql");
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '7640',
   database: 'mysqlDB',
-})
+});
 
 con.connect(function (err) {
   if (err) {
     console.log(err);
+    return;
   }
-  else {
-    var sql = "CREATE TABLE mytable (id int,name varchar(10))";
 
-    con.query(sql, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("table created");
-      }
-    })
-  }
-})
+  var sql = "CREATE TABLE IF NOT EXISTS consultation (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))";
+
+  con.query(sql, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("table created or already exists");
+  });
+});
+
+app.post('/submit', function (req, res) {
+  let sql = "INSERT INTO consultation (name, email) VALUES (?, ?)";
+  let values = [req.body.name, req.body.email];
+
+  con.query(sql, values, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(500).send('An error occurred during the query');
+    } else {
+      res.send('Data inserted successfully');
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port:3000");
+});
